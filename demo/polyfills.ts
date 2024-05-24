f1ae78cc52cbaef8e998c113ed737939c5165b82
Pierre Clavequin <pierre.clavequin@valueapex.com>
2024-05-24 14:10:12 +0800

    chore: update to Angular 17

--- demo/polyfills.ts
+++ demo/polyfills.ts
@@ -53,7 +53,8 @@
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';
+import 'zone.js/testing';
 
 
 
