f1acce3aafc12a59c56b7542a444c5b0de846aad
Jim <38460550+jjmhalew@users.noreply.github.com>
2024-10-12 14:37:59 +0200

    provideExperimentalZonelessChangeDetection()

--- demo/main.ts
+++ demo/main.ts
@@ -1,8 +1,9 @@
+import { provideExperimentalZonelessChangeDetection } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { AppModule } from "./app.module";
 
 bootstrapApplication(AppComponent, {
-  providers: [AppModule],
+  providers: [AppModule, provideExperimentalZonelessChangeDetection()],
 });
