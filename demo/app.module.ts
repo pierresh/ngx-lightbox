0a35858968127a16e33d9310bfba778c6d4568c6
Jim <38460550+jjmhalew@users.noreply.github.com>
2024-10-12 17:14:00 +0200

    remove app module

--- demo/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-
-import { LightboxModule } from "../src";
-
-@NgModule({
-  imports: [BrowserModule, LightboxModule],
-})
-export class AppModule {}
--- demo/main.ts
+++ demo/main.ts
@@ -2,8 +2,7 @@ import { provideExperimentalZonelessChangeDetection } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { AppModule } from "./app.module";
 
 bootstrapApplication(AppComponent, {
-  providers: [AppModule, provideExperimentalZonelessChangeDetection()],
+  providers: [provideExperimentalZonelessChangeDetection()],
 });
